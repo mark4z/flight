af9b7182a396c559fbdc4e0fa245601e17b6251e
c <anymre@outlook.com>
2019-09-28 22:37:49 +0800

    1.3

--- main.go
+++ main.go
@@ -5,8 +5,7 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/toolbox"
 	_ "github.com/go-sql-driver/mysql"
-	_ "i/flight/routers"
-	"i/flight/service"
+	"i/service"
 	"time"
 )
 
